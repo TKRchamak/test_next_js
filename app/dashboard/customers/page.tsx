241e1ec1e138aae988c6a9151e6a0fbcd309d40e
TKRchamak <tonmoykumarroy6@gmail.com>
2023-11-20 18:20:01 +0600

    all

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <p>Customers Page</p>
+      {/* <div
+        style={{
+          height: '400px',
+          width: '300px',
+          // border: '2px solid red',
+          borderRadius: '1rem',
+          backgroundImage:
+            'linear-gradient(45deg, rgba(198, 131, 215, 0.2), rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5), rgba(255, 199, 199, 0.2))',
+          boxShadow:
+            'rgb(204, 219, 232) 3px 3px 6px 0px, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px',
+        }}
+      ></div> */}
+    </div>
+  );
+};
+
+export default page;
